f2b0357194c6e634d709c44b4dce1974779bb57c
ruhui <921263933@qq.com>
2019-03-06 18:44:46 +0800

    Initial commit

---
+++ banner/src/main/java/com/youth/banner/view/BannerViewPager.java
@@ -0,0 +1,91 @@
+package com.youth.banner.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class BannerViewPager extends ViewPager {
+    private boolean scrollable = true;
+
+    /****
+     * 滑动距离及坐标 归还父控件焦点
+     ****/
+    private float xDistance, yDistance, xLast, yLast;
+    /**
+     * 是否是左右滑动
+     **/
+    private boolean mIsBeingDragged = true;
+
+    public BannerViewPager(Context context) {
+        super(context);
+    }
+
+    public BannerViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if(this.scrollable) {
+            if (getCurrentItem() == 0 && getChildCount() == 0) {
+                return false;
+            }
+            return super.onTouchEvent(ev);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(this.scrollable) {
+            if (getCurrentItem() == 0 && getChildCount() == 0) {
+                return false;
+            }
+            return super.onInterceptTouchEvent(ev);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        getParent().requestDisallowInterceptTouchEvent(true);
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                xDistance = yDistance = 0f;
+                xLast = ev.getX();
+                yLast = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float curX = ev.getX();
+                final float curY = ev.getY();
+                xDistance += Math.abs(curX - xLast);
+                yDistance += Math.abs(curY - yLast);
+                xLast = curX;
+                yLast = curY;
+                if (!mIsBeingDragged) {
+                    if (yDistance < xDistance ) {//小于30度都左右滑
+                        mIsBeingDragged = true;
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                    } else {
+                        getParent().requestDisallowInterceptTouchEvent(false);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                mIsBeingDragged = false;
+                getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+
+    public void setScrollable(boolean scrollable) {
+        this.scrollable = scrollable;
+    }
+}
